bd9edc604a5de5b3cd379d6fb158443f757e94e7
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-19 23:39:25 +0500

    INFRA: Initialize API Project

---
+++ LibraryManagement.Api/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿//-----------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Build Reliable Library Management Solutions
+//-----------------------------------------------------------
+
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace LibraryManagement.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : RESTFulController
+    {
+        [HttpGet]
+        public IActionResult GetGreeting() => Ok("Hello, Library!");
+    }
+}
