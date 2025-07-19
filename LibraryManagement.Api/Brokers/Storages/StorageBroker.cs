8a87e1aa7f42fe94886f6de48b7a7309932038ef
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-20 00:30:55 +0500

    DATA: Model & Migrate Books

---
+++ LibraryManagement.Api/Brokers/Storages/StorageBroker.cs
@@ -0,0 +1,31 @@
+﻿//-----------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Build Reliable Library Management Solutions
+//-----------------------------------------------------------
+
+using EFxceptions;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagement.Api.Brokers.Storages
+{
+    public partial class StorageBroker : EFxceptionsContext
+    {
+        private readonly IConfiguration configuration;
+
+        public StorageBroker(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            this.Database.Migrate();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString =
+                this.configuration.GetConnectionString(name: "DefaultConnection");
+
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        public override void Dispose() { }
+    }
+}
---
+++ LibraryManagement.Api/Models/Foundations/Books/Book.cs
@@ -0,0 +1,16 @@
+﻿//-----------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Build Reliable Library Management Solutions
+//-----------------------------------------------------------
+
+namespace LibraryManagement.Api.Models.Foundations.Books
+{
+    public class Book
+    {
+        public Guid BookId { get; set; }
+        public Guid ReaderId { get; set; }
+        public string BookTitle { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+    }
+}
