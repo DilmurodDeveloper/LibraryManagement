4f324c1dff1a570f7b1c7179cb04bd31613b13fb
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-20 19:36:01 +0500

    CODE RUB: Make ReaderId optional in Book model

--- LibraryManagement.Api/Models/Foundations/Books/Book.cs
+++ LibraryManagement.Api/Models/Foundations/Books/Book.cs
@@ -3,6 +3,7 @@
 // Free To Use To Build Reliable Library Management Solutions
 //-----------------------------------------------------------
 
+using System.Text.Json.Serialization;
 using LibraryManagement.Api.Models.Foundations.Readers;
 
 namespace LibraryManagement.Api.Models.Foundations.Books
@@ -10,10 +11,12 @@ namespace LibraryManagement.Api.Models.Foundations.Books
     public class Book
     {
         public Guid BookId { get; set; }
-        public Guid ReaderId { get; set; }
         public string BookTitle { get; set; }
         public string Author { get; set; }
         public string Genre { get; set; }
-        public Reader Reader { get; set; }
+        [JsonIgnore]
+        public Guid? ReaderId { get; set; }
+        [JsonIgnore]
+        public Reader? Reader { get; set; }
     }
 }
