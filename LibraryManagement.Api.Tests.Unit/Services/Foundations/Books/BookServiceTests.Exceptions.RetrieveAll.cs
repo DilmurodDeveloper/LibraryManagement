27d435efc158d2e969f4e70d1e34b0a32dc62c78
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-20 22:11:30 +0500

    ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSqlExceptionOccursAndLogIt -> FAIL

---
+++ LibraryManagement.Api.Tests.Unit/Services/Foundations/Books/BookServiceTests.Exceptions.RetrieveAll.cs
@@ -0,0 +1,55 @@
+﻿//-----------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Build Reliable Library Management Solutions
+//-----------------------------------------------------------
+
+using FluentAssertions;
+using LibraryManagement.Api.Models.Foundations.Books.Exceptions;
+using Microsoft.Data.SqlClient;
+using Moq;
+
+namespace LibraryManagement.Api.Tests.Unit.Services.Foundations.Books
+{
+    public partial class BookServiceTests
+    {
+        [Fact]
+        public void ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSqlExceptionOccursAndLogIt()
+        {
+            // given
+            SqlException sqlException = GetSqlError();
+
+            var failedBookStorageException =
+                new FailedBookStorageException(sqlException);
+
+            var expectedBookDependencyException =
+                new BookDependencyException(failedBookStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllBooks())
+                    .Throws(sqlException);
+
+            // when
+            Action retrieveAllBooksAction = () =>
+                this.bookService.RetrieveAllBooks();
+
+            BookDependencyException actualBookDependencyException =
+                Assert.Throws<BookDependencyException>(retrieveAllBooksAction);
+
+            // then
+            actualBookDependencyException.Should()
+                .BeEquivalentTo(expectedBookDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllBooks(),
+                    Times.Once());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedBookDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
