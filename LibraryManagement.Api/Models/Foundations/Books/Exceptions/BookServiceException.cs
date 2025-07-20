fe03d0452f19b10715116093f2adc02d49f9475b
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-20 16:59:25 +0500

    ShouldThrowServiceExceptionOnAddIfServiceErrorOccursAndLogItAsync -> FAIL

---
+++ LibraryManagement.Api/Models/Foundations/Books/Exceptions/BookServiceException.cs
@@ -0,0 +1,17 @@
+﻿//-----------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Build Reliable Library Management Solutions
+//-----------------------------------------------------------
+
+using Xeptions;
+
+namespace LibraryManagement.Api.Models.Foundations.Books.Exceptions
+{
+    public class BookServiceException : Xeption
+    {
+        public BookServiceException(Xeption innerException)
+            : base(message: "Book service error occurred, contact support.",
+                  innerException)
+        { }
+    }
+}
---
+++ LibraryManagement.Api/Models/Foundations/Books/Exceptions/FailedBookServiceException.cs
@@ -0,0 +1,17 @@
+﻿//-----------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Build Reliable Library Management Solutions
+//-----------------------------------------------------------
+
+using Xeptions;
+
+namespace LibraryManagement.Api.Models.Foundations.Books.Exceptions
+{
+    public class FailedBookServiceException : Xeption
+    {
+        public FailedBookServiceException(Exception innerException)
+            : base(message: "Failed book service error occurred, contact support.",
+                  innerException)
+        { }
+    }
+}
