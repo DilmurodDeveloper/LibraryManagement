28a8839a9bebf2c8fea2e5c187e7ff6a387fe6db
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-21 00:52:42 +0500

    ShouldThrowServiceExceptionOnRemoveIfExceptionOccursAndLogItAsync -> FAIL

--- LibraryManagement.Api.Tests.Unit/Services/Foundations/Books/BookServiceTests.Exceptions.RemoveById.cs
+++ LibraryManagement.Api.Tests.Unit/Services/Foundations/Books/BookServiceTests.Exceptions.RemoveById.cs
@@ -101,5 +101,47 @@ namespace LibraryManagement.Api.Tests.Unit.Services.Foundations.Books
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnRemoveIfExceptionOccursAndLogItAsync()
+        {
+            // given
+            Guid someBookId = Guid.NewGuid();
+            var serviceException = new Exception();
+
+            var failedBookServiceException =
+                new FailedBookServiceException(serviceException);
+
+            var expectedBookServiceException =
+                new BookServiceException(failedBookServiceException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectBookByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(serviceException);
+
+            // when
+            ValueTask<Book> removeBookByIdTask =
+                this.bookService.RemoveBookByIdAsync(someBookId);
+
+            BookServiceException actualBookServiceException =
+                await Assert.ThrowsAsync<BookServiceException>(() =>
+                    removeBookByIdTask.AsTask());
+
+            // then
+            actualBookServiceException.Should()
+                .BeEquivalentTo(expectedBookServiceException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectBookByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedBookServiceException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
