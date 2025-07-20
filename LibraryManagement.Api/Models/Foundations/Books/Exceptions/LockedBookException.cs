bc8946e5d7678674a550dac7d44906e20ad0a60c
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-21 00:07:42 +0500

    ShouldThrowDependencyValidationExceptionOnModifyIfDatabaseUpdateConcurrencyErrorOccursAndLogItAsync -> FAIL

---
+++ LibraryManagement.Api/Models/Foundations/Books/Exceptions/LockedBookException.cs
@@ -0,0 +1,17 @@
+﻿//-----------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Build Reliable Library Management Solutions
+//-----------------------------------------------------------
+
+using Xeptions;
+
+namespace LibraryManagement.Api.Models.Foundations.Books.Exceptions
+{
+    public class LockedBookException : Xeption
+    {
+        public LockedBookException(Exception innerException)
+            : base(message: "Book is locked, please try again later.",
+                  innerException)
+        { }
+    }
+}
