5780055c9067b9c76df9444b1e699ff343c86990
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-21 17:31:27 +0500

    ShouldThrowDependencyValidationExceptionOnModifyIfDatabaseUpdateConcurrencyErrorOccursAndLogItAsync -> FAIL

--- LibraryManagement.Api.Tests.Unit/Services/Foundations/Readers/ReaderServiceTests.Exceptions.Modify.cs
+++ LibraryManagement.Api.Tests.Unit/Services/Foundations/Readers/ReaderServiceTests.Exceptions.Modify.cs
@@ -109,5 +109,53 @@ namespace LibraryManagement.Api.Tests.Unit.Services.Foundations.Readers
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowDependencyValidationExceptionOnModifyIfDatabaseUpdateConcurrencyErrorOccursAndLogItAsync()
+        {
+            // given
+            Reader randomReader = CreateRandomReader();
+            Reader someReader = randomReader;
+            Guid readerId = someReader.ReaderId;
+            var dbUpdateConcurrencyException = new DbUpdateConcurrencyException();
+
+            var lockedReaderException =
+                new LockedReaderException(dbUpdateConcurrencyException);
+
+            var expectedReaderDependencyValidationException =
+                new ReaderDependencyValidationException(lockedReaderException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectReaderByIdAsync(readerId))
+                    .Throws(dbUpdateConcurrencyException);
+
+            // when
+            ValueTask<Reader> modifyReaderTask =
+                this.readerService.ModifyReaderAsync(someReader);
+
+            ReaderDependencyValidationException actualReaderDependencyValidationException =
+                await Assert.ThrowsAsync<ReaderDependencyValidationException>(() =>
+                    modifyReaderTask.AsTask());
+
+            // then
+            actualReaderDependencyValidationException.Should()
+                .BeEquivalentTo(expectedReaderDependencyValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedReaderDependencyValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectReaderByIdAsync(readerId),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateReaderAsync(someReader),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
---
+++ LibraryManagement.Api/Models/Foundations/Readers/Exceptions/LockedReaderException.cs
@@ -0,0 +1,17 @@
+﻿//-----------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Build Reliable Library Management Solutions
+//-----------------------------------------------------------
+
+using Xeptions;
+
+namespace LibraryManagement.Api.Models.Foundations.Readers.Exceptions
+{
+    public class LockedReaderException : Xeption
+    {
+        public LockedReaderException(Exception innerException)
+            : base(message: "Reader is locked, please try again later.",
+                  innerException)
+        { }
+    }
+}
