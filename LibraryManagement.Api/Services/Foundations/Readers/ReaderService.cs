d67223dd33522f2dfe482e02df33d8421c99dc23
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-21 18:44:55 +0500

    ShouldThrowServiceExceptionOnRemoveIfExceptionOccursAndLogItAsync -> PASS

--- LibraryManagement.Api/Services/Foundations/Readers/ReaderService.cs
+++ LibraryManagement.Api/Services/Foundations/Readers/ReaderService.cs
@@ -117,6 +117,18 @@ namespace LibraryManagement.Api.Services.Foundations.Readers
 
                 throw readerDependencyException;
             }
+            catch (Exception exception)
+            {
+                var failedReaderServiceException =
+                    new FailedReaderServiceException(exception);
+
+                var readerServiceException =
+                    new ReaderServiceException(failedReaderServiceException);
+
+                this.loggingBroker.LogError(readerServiceException);
+
+                throw readerServiceException;
+            }
         }
     }
 }
