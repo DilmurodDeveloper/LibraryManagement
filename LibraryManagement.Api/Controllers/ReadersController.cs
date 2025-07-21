7b6d453be66f20714d346b2505fa812b4a61f371
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-21 14:05:26 +0500

    EXPOSERS: Post Reader

---
+++ LibraryManagement.Api/Controllers/ReadersController.cs
@@ -0,0 +1,58 @@
+﻿//-----------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Build Reliable Library Management Solutions
+//-----------------------------------------------------------
+
+using LibraryManagement.Api.Models.Foundations.Readers;
+using LibraryManagement.Api.Models.Foundations.Readers.Exceptions;
+using LibraryManagement.Api.Services.Foundations.Readers;
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace LibraryManagement.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ReadersController : RESTFulController
+    {
+        private readonly IReaderService readerService;
+
+        public ReadersController(IReaderService readerService)
+        {
+            this.readerService = readerService;
+        }
+
+        [HttpPost]
+        public async ValueTask<ActionResult<Reader>> PostReaderAsync(Reader reader)
+        {
+            try
+            {
+                Reader postedReader =
+                    await this.readerService.AddReaderAsync(reader);
+
+                return Created(postedReader);
+            }
+            catch (ReaderValidationException readerValidationException)
+            {
+                return BadRequest(readerValidationException.InnerException);
+            }
+            catch (ReaderDependencyValidationException readerDependencyValidationException)
+                when (readerDependencyValidationException.InnerException is AlreadyExistsReaderException)
+            {
+                return Conflict(readerDependencyValidationException.InnerException);
+            }
+            catch (ReaderDependencyValidationException readerDependencyValidationException)
+            {
+                return BadRequest(readerDependencyValidationException.InnerException);
+            }
+            catch (ReaderDependencyException readerDependencyException)
+            {
+                return InternalServerError(readerDependencyException.InnerException);
+            }
+            catch (ReaderServiceException readerServiceException)
+            {
+                return InternalServerError(readerServiceException.InnerException);
+            }
+        }
+    }
+}
