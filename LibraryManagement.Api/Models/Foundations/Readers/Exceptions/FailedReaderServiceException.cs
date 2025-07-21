628725696d98c25760e8c0111089b10022b10761
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-21 13:38:49 +0500

    ShouldThrowServiceExceptionOnAddIfServiceErrorOccursAndLogItAsync -> FAIL

---
+++ LibraryManagement.Api/Models/Foundations/Readers/Exceptions/FailedReaderServiceException.cs
@@ -0,0 +1,17 @@
+﻿//-----------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Build Reliable Library Management Solutions
+//-----------------------------------------------------------
+
+using Xeptions;
+
+namespace LibraryManagement.Api.Models.Foundations.Readers.Exceptions
+{
+    public class FailedReaderServiceException : Xeption
+    {
+        public FailedReaderServiceException(Exception innerException)
+            : base(message: "Failed reader service error occurred, contact support.",
+                  innerException)
+        { }
+    }
+}
