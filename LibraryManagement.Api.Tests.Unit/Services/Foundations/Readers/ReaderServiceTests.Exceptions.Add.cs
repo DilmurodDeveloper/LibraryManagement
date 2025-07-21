a87b7b6e271a81be0e20b98da66706c309dfc8b8
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-21 13:33:10 +0500

    ShouldThrowDependencyValidationOnAddIfDuplicateKeyErrorOccursAndLogItAsync -> FAIL

--- LibraryManagement.Api.Tests.Unit/Services/Foundations/Readers/ReaderServiceTests.Exceptions.Add.cs
+++ LibraryManagement.Api.Tests.Unit/Services/Foundations/Readers/ReaderServiceTests.Exceptions.Add.cs
@@ -3,6 +3,7 @@
 // Free To Use To Build Reliable Library Management Solutions
 //-----------------------------------------------------------
 
+using EFxceptions.Models.Exceptions;
 using LibraryManagement.Api.Models.Foundations.Readers;
 using LibraryManagement.Api.Models.Foundations.Readers.Exceptions;
 using Microsoft.Data.SqlClient;
@@ -44,6 +45,47 @@ namespace LibraryManagement.Api.Tests.Unit.Services.Foundations.Readers
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogCritical(It.Is(SameExceptionAs(
                     expectedReaderDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyValidationOnAddIfDuplicateKeyErrorOccursAndLogItAsync()
+        {
+            // given
+            Reader someReader = CreateRandomReader();
+            string someMessage = GetRandomString();
+
+            var duplicateKeyException =
+                new DuplicateKeyException(someMessage);
+
+            var alreadyExistsReaderException =
+                new AlreadyExistsReaderException(duplicateKeyException);
+
+            var expectedReaderDependencyValidationException =
+                new ReaderDependencyValidationException(alreadyExistsReaderException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.InsertReaderAsync(someReader))
+                    .ThrowsAsync(duplicateKeyException);
+
+            // when
+            ValueTask<Reader> addReaderTask =
+                this.readerService.AddReaderAsync(someReader);
+
+            // then
+            await Assert.ThrowsAsync<ReaderDependencyValidationException>(() =>
+                addReaderTask.AsTask());
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertReaderAsync(someReader),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedReaderDependencyValidationException))),
                         Times.Once);
 
             this.storageBrokerMock.VerifyNoOtherCalls();
---
+++ LibraryManagement.Api/Models/Foundations/Readers/Exceptions/AlreadyExistsReaderException.cs
@@ -0,0 +1,16 @@
+﻿//-----------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Build Reliable Library Management Solutions
+//-----------------------------------------------------------
+
+using Xeptions;
+
+namespace LibraryManagement.Api.Models.Foundations.Readers.Exceptions
+{
+    public class AlreadyExistsReaderException : Xeption
+    {
+        public AlreadyExistsReaderException(Exception innerException)
+            : base(message: "Reader already exists.", innerException)
+        { }
+    }
+}
