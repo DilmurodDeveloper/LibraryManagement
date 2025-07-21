5e68cf4d6bd96a84bedc4e84ddf06633836c69e9
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-21 17:25:16 +0500

    ShouldThrowCriticalDependencyExceptionOnModifyIfSqlErrorOccursAndLogItAsync -> PASS

--- LibraryManagement.Api/Services/Foundations/Readers/ReaderService.cs
+++ LibraryManagement.Api/Services/Foundations/Readers/ReaderService.cs
@@ -7,6 +7,7 @@ using LibraryManagement.Api.Brokers.Loggings;
 using LibraryManagement.Api.Brokers.Storages;
 using LibraryManagement.Api.Models.Foundations.Readers;
 using LibraryManagement.Api.Models.Foundations.Readers.Exceptions;
+using Microsoft.Data.SqlClient;
 
 namespace LibraryManagement.Api.Services.Foundations.Readers
 {
@@ -86,6 +87,18 @@ namespace LibraryManagement.Api.Services.Foundations.Readers
                 this.loggingBroker.LogError(readerValidationException);
 
                 throw readerValidationException;
+            }
+            catch (SqlException sqlException)
+            {
+                var failedReaderStorageException =
+                    new FailedReaderStorageException(sqlException);
+
+                var readerDependencyException =
+                    new ReaderDependencyException(failedReaderStorageException);
+
+                this.loggingBroker.LogCritical(readerDependencyException);
+
+                throw readerDependencyException;
             }
         }
     }
