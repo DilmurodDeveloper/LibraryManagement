a5be4a77bf0e5a51850672c493155e65f57fe437
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-21 12:34:10 +0500

    ShouldThrowValidationExceptionOnAddIfReaderIsInvalidAndLogItAsync -> FAIL

---
+++ LibraryManagement.Api/Models/Foundations/Readers/Exceptions/InvalidReaderException.cs
@@ -0,0 +1,16 @@
+﻿//-----------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Build Reliable Library Management Solutions
+//-----------------------------------------------------------
+
+using Xeptions;
+
+namespace LibraryManagement.Api.Models.Foundations.Readers.Exceptions
+{
+    public class InvalidReaderException : Xeption
+    {
+        public InvalidReaderException()
+            : base(message: "Reader is invalid.")
+        { }
+    }
+}
