6cd3869fa8eb02cec5cbe1571a1f0be1bd4d7e54
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-21 17:40:40 +0500

    CODE RUB: Implement TryCatch

--- LibraryManagement.Api/Services/Foundations/Readers/ReaderService.cs
+++ LibraryManagement.Api/Services/Foundations/Readers/ReaderService.cs
@@ -6,9 +6,6 @@
 using LibraryManagement.Api.Brokers.Loggings;
 using LibraryManagement.Api.Brokers.Storages;
 using LibraryManagement.Api.Models.Foundations.Readers;
-using LibraryManagement.Api.Models.Foundations.Readers.Exceptions;
-using Microsoft.Data.SqlClient;
-using Microsoft.EntityFrameworkCore;
 
 namespace LibraryManagement.Api.Services.Foundations.Readers
 {
@@ -49,94 +46,17 @@ namespace LibraryManagement.Api.Services.Foundations.Readers
             return maybeReader;
         });
 
-        public async ValueTask<Reader> ModifyReaderAsync(Reader reader)
+        public ValueTask<Reader> ModifyReaderAsync(Reader reader) =>
+        TryCatch(async () =>
         {
-            try
-            {
-                ValidateReaderOnModify(reader);
+            ValidateReaderOnModify(reader);
 
-                Reader maybeReader =
-                    await this.storageBroker.SelectReaderByIdAsync(reader.ReaderId);
+            Reader maybeReader =
+                await this.storageBroker.SelectReaderByIdAsync(reader.ReaderId);
 
-                ValidateAgainstStorageReaderOnModify(reader, maybeReader);
+            ValidateAgainstStorageReaderOnModify(reader, maybeReader);
 
-                return await this.storageBroker.UpdateReaderAsync(reader);
-            }
-            catch (NullReaderException nullReaderException)
-            {
-                var readerValidationException =
-                    new ReaderValidationException(nullReaderException);
-
-                this.loggingBroker.LogError(readerValidationException);
-
-                throw readerValidationException;
-            }
-            catch (InvalidReaderException invalidReaderException)
-            {
-                var readerValidationException =
-                    new ReaderValidationException(invalidReaderException);
-
-                this.loggingBroker.LogError(readerValidationException);
-
-                throw readerValidationException;
-            }
-            catch (NotFoundReaderException notFoundReaderException)
-            {
-                var readerValidationException =
-                    new ReaderValidationException(notFoundReaderException);
-
-                this.loggingBroker.LogError(readerValidationException);
-
-                throw readerValidationException;
-            }
-            catch (SqlException sqlException)
-            {
-                var failedReaderStorageException =
-                    new FailedReaderStorageException(sqlException);
-
-                var readerDependencyException =
-                    new ReaderDependencyException(failedReaderStorageException);
-
-                this.loggingBroker.LogCritical(readerDependencyException);
-
-                throw readerDependencyException;
-            }
-            catch (DbUpdateConcurrencyException dbUpdateConcurrencyException)
-            {
-                var lockedReaderException =
-                    new LockedReaderException(dbUpdateConcurrencyException);
-
-                var readerDependencyValidationException =
-                    new ReaderDependencyValidationException(lockedReaderException);
-
-                this.loggingBroker.LogError(readerDependencyValidationException);
-
-                throw readerDependencyValidationException;
-            }
-            catch (DbUpdateException dbUpdateException)
-            {
-                var failedReaderStorageException =
-                    new FailedReaderStorageException(dbUpdateException);
-
-                var readerDependencyException =
-                    new ReaderDependencyException(failedReaderStorageException);
-
-                this.loggingBroker.LogError(readerDependencyException);
-
-                throw readerDependencyException;
-            }
-            catch (Exception exception)
-            {
-                var failedReaderServiceException =
-                    new FailedReaderServiceException(exception);
-
-                var readerServiceException =
-                    new ReaderServiceException(failedReaderServiceException);
-
-                this.loggingBroker.LogError(readerServiceException);
-
-                throw readerServiceException;
-            }
-        }
+            return await this.storageBroker.UpdateReaderAsync(reader);
+        });
     }
 }
