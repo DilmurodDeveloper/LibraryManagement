468c7b0b93370e425e1b434106adab14288adce6
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-21 19:14:04 +0500

    FOUNDATIONS: Retrieve ReaderBook By Id

---
+++ LibraryManagement.Api/Services/Foundations/ReaderBooks/IReaderBookService.cs
@@ -0,0 +1,14 @@
+﻿//-----------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Build Reliable Library Management Solutions
+//-----------------------------------------------------------
+
+using LibraryManagement.Api.Models.Foundations.ReaderBooks;
+
+namespace LibraryManagement.Api.Services.Foundations.ReaderBooks
+{
+    public interface IReaderBookService
+    {
+        ValueTask<ReaderBook> RetrieveReaderBookByIdAsync(Guid readerId);
+    }
+}
